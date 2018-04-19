bb5659cacb2e2049cde24dc71a49f5500f77b4cf
Harsh Agrawal <harsh@vinsol.com>
2018-04-19 17:58:02 +0530

    Add spree admin insights extension

--- vendor/assets/javascripts/spree/backend/all.js
+++ vendor/assets/javascripts/spree/backend/all.js
@@ -10,3 +10,4 @@
 
 //= require_tree .
 //= require spree/backend/spree_events_tracker
+//= require spree/backend/spree_admin_insights
--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -12,3 +12,4 @@
 //= require_tree .
 //= require spree/frontend/spree_auth
 //= require spree/frontend/spree_events_tracker
+//= require spree/frontend/spree_admin_insights
