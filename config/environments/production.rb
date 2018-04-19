5712ccfea6899ec3a84f1360cc06e96809c9efb1
Himanshu <himanshumishra3@gmail.com>
2018-04-19 18:45:01 +0530

    smtp settings added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,17 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => nil,
+    :password => nil,
+    :address => 'localhost',
+    :domain => 'localhost.localdomain',
+    :port => 25,
+    :authentication => nil,
+    :enable_starttls_auto => true
+  }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
