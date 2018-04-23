5479f76ab700fca5fa031faf0727865900fa76a4
Himanshu <himanshumishra3@gmail.com>
2018-04-23 15:28:07 +0530

    gem updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from Taxjar::Error, with: :taxjar_rollback
+
+  private
+    def taxjar_rollback(e)
+      flash[:error] = 'TaxJar::' + e.message
+      redirect_to cart_path
+    end
 end
