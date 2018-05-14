c52310d300aef94db7b70b2604cbc9a000bf8804
Himanshu <himanshumishra3@gmail.com>
2018-05-14 15:34:15 +0530

    rails 12 factor removed

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module HerokuMasterApp
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
