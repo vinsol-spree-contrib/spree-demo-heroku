07402729cb6bb8a123226d84a223e19418fa0d13
Himanshu <himanshumishra3@gmail.com>
2018-05-21 17:28:14 +0530

    json file updated

---
+++ public/vinsol_spree_themes/theme-BigShop-3-3-bump/javascripts/default.manifest.js
@@ -0,0 +1,36 @@
+// all javascript files related to themes should be require in this manifest.
+//= require slick.min
+
+
+$(document).on('ready', function() {
+
+  // Home Page Slider
+  $(".regular").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  // All Responsive Tables
+  $( ".table" ).wrap( "<div class='table-responsive'></div>" );
+
+  // For Header Responsive
+  $(".search_toggle").click(function(){
+    $(".search_area").toggleClass("search_open");
+    $("body").toggleClass("modal-open");
+    $(".responsive-overlay").toggleClass("open");
+  });
+
+  $(".userbar_toggle").click(function(){
+    $("#top-nav-bar").toggleClass("userbar_open");
+    $("body").toggleClass("modal-open");
+    $(".responsive-overlay").toggleClass("open");
+  });
+
+  $(".sidebar_toggle").click(function(){
+    $("#sidebar").toggleClass("sidebar_open");
+    $("body").toggleClass("modal-open");
+    $(".responsive-overlay").toggleClass("open");
+  });
+
+});
