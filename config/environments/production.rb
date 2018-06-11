736331b0cbd8a84016b140eb15748294132aaba5
Himanshu <himanshumishra3@gmail.com>
2018-06-11 18:38:51 +0530

    gem updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
