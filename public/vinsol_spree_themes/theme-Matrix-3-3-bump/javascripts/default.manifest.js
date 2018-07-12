93d4feb94b955607ee1ced8b0d7cb6f449a9fc59
Himanshu <himanshumishra3@gmail.com>
2018-07-13 01:42:50 +0530

    theme updated

--- public/vinsol_spree_themes/theme-Matrix-3-3-bump/javascripts/default.manifest.js
+++ public/vinsol_spree_themes/theme-Matrix-3-3-bump/javascripts/default.manifest.js
@@ -1,6 +1,12 @@
 // all javascript files related to themes should be require in this manifest.
+//= require jquery_zoom_min
+
 $( document ).ready(function() {
   $( ".number_field_tag" ).spinner({
     min: 1
   });
+  $('#product_zoom').zoom();
+  $('li.tmb-all > a').on('click', function(){
+    $('#product_zoom').zoom();
+  })
 });
