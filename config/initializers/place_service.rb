d0e873f2de66debb3feb0935b27d5cc17f4d9f6f
Shivam Jain <shivam@vinsol.com>
2018-09-13 19:06:30 +0530

    update spree_intercom

---
+++ config/initializers/place_service.rb
@@ -0,0 +1,10 @@
+Spree::Intercom::Events::Order::PlaceService.class_eval do
+  private
+    def host_name
+      "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
+    end
+
+    def protocol_name
+      'https'
+    end
+end
